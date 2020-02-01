2dad20c70b7f1b58f493235c0b221662d6d35c22
Morgane Dinse <morgane.dinse@gmail.com>
2020-02-01 16:09:20 -0500

    connection for my views pages 'index' and 'new' is done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,35 @@
 class CocktailsController < ApplicationController
+
+  before_action :cocktail_find, only: [:show, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_params)
+    redirect_to cocktail_path(@new_cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.destroy
+    redirect_to cocktail_path(@cocktails)
+  end
+
+  private
+
+  def cocktail_find
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
