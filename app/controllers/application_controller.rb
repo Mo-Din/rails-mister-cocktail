cfaa6a9d1da6e9dbde3dcfbdbff68acf64600d28
Morgane Dinse <morgane.dinse@gmail.com>
2020-02-01 15:32:30 -0500

    controller Cocktail added definition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+
+  before_action :cocktail_param, only: [:show, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktails = Cocktail.new
+  end
+
+  def create
+    @cocktails = Cocktail.create(cocktail_param)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktails = Cocktail.destroy
+  end
+
+  private
+
+  def cocktail_param
+    @cocktails = Cocktail.find(params[:id])
+  end
 end
